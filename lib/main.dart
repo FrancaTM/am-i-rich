48bdd896676aac8a0c35b23ea0efb1b129e41a3f
Túlio <francatm@outlook.com>
2019-05-12 08:59:42 -0300

    Add scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello world"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[300],
+        appBar: AppBar(
+          title: Text('Am I Rich?'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://grayline.is/files/5415/3493/3126/northern-lights-deluxe-in-iceland.jpg')),
+        ),
       ),
     ),
   );
